0249c2bf25165e55035274baa81e3954c2be5756
Wangjie <1820089619@qq.com>
2018-01-08 11:39:44 +0800

    显示英雄列表，选择英雄后可现实英雄详情

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,102 @@
 import { Component } from '@angular/core';
-
+export class Hero {
+  id: number;
+  name: string;
+}
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr.  Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' },
+];
 @Component({
   selector: 'app-root',
+  styles: [`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .heroes {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .heroes li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .heroes li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .heroes li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+    .heroes .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css'],
+  template: `  
+    <h1> {{title}}</h1>
+    <div *ngIf="selectedHero">
+      <h2> {{selectedHero.name}} details!</h2>
+      <div><label>id:  </label>{{selectedHero.id}}</div>
+      <div>
+        <label>name:  </label> 
+        <input [(ngModel)]="selectedHero.name" placeholder="name">
+      </div>
+    </div>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes" 
+      (click)="onSelect(hero)" 
+      [class.selected]="hero === selectedHero">
+      <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    <ul>
+    `
+  //反引号包裹的组件模板能让你把<h1>、<h2>和<div>元素各自放在一行上
+  //let hero of heroes:从heroes数组中取出每个英雄，存入一个局部的hero变量，并让它在相应的模板实例中可用
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes!';
+  selectedHero: Hero;
+  heroes = HEROES;
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
+  
 }
+
